e66f7d7dbb89be97a7398501c1ed7ea09aaf9bf1
Gamalina <danielrudcallesen96@gmail.com>
2022-09-05 13:50:08 +0200

    Task 2

---
+++ ClassRoomNet60/Model/Classroom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60.Model
+{
+    public class Classroom
+    {
+        public string _className;
+        public List<Classroom> _classList;
+        public DateTime _SemesterStart;
+
+        public Classroom(string className, List<Classroom> classList, DateTime semesterStart)
+        {
+            _className = className;
+            _classList = classList;
+            _SemesterStart = semesterStart;
+        }
+
+        public string ClassName
+        {
+            get => _className;
+            set => _className = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public List<Classroom> ClassList
+        {
+            get => _classList;
+            set => _classList = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public DateTime SemesterStart
+        {
+            get => _SemesterStart;
+            set => _SemesterStart = value;
+        }
+    }
+}
